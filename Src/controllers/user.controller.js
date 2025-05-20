84472122739982fe6866e6eac570044b70837989
felipegit@outlook.com <felipegit@outlook.com>
2025-05-19 21:54:31 -0500

    Agregar funciones getOne, create, update y delete en el controlador de usuarios

--- Src/controllers/user.controller.js
+++ Src/controllers/user.controller.js
@@ -22,3 +22,71 @@ const getAll = async (req, res, next) => {
     response.success(req, res, data, 200);
   } catch (error) {}
 };
+
+const getOne = async (req, res, next) => {
+    try {
+        const user = await User.findOne({where: {id},include: { model: Rol, as: "Role" }});
+        let data = "";
+        if (user.length === 0) {
+        data = {
+            total_registered: user.length,
+            data: user,
+        };
+        } else {
+        data = {
+            message: "Table dont have records",
+        };
+        }
+        response.success(req, res, data, 200);
+    } catch (error) {}
+}
+
+const create = async (req, res, next) => {
+    const data = req.body;
+    await User.sync();
+    const created=await User.create(data);
+    message={
+        message: "User created",
+        data: created.id,
+    };
+    let createdAuth = "";
+    if(data.emaiel && data.password){
+       await Auth.sync();
+       password = await bycrypt.hash(data.password.ToString(), 10);
+       createdAuth = await Auth.create({
+            id: created.id,
+            email: data.email,
+            password: password,
+        });
+        message.AuthID=createdAuth.id;
+    }
+    response.success(req, res, message, 201);
+}
+const update = async(req,res,next)=>{
+    try {
+        const data = req.body;
+        const id = req.params.id;
+        const updated = await User.update(data,{where: {id}});
+        message = {
+            msg: "Record was Updated successfully",
+            regID: id
+        }
+        response.success(req,res,message,200);
+    } catch (error) {
+        next(error)
+    }
+}
+
+const deleted = async(req,res,next)=>{
+    try {
+        const id = req.params.id;
+        const deleted = await User.destroy({ where: { id } });
+        message = {
+            msg: "Record was Updated successfully",
+            regID: id
+        }
+        response.success(req,res,message,200);
+    } catch (error) {
+        next(error)
+    }
+}

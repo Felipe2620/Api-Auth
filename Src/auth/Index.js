1eefe9dd4567e2c88d1c9d7033735d4206f1f88d
felipegit@outlook.com <felipegit@outlook.com>
2025-05-19 21:09:16 -0500

    Implementación de funciones para asignar y verificar tokens JWT, y manejo de autorización en el middleware.

--- Src/auth/Index.js
+++ Src/auth/Index.js
@@ -1,1 +1,34 @@
-const config = requiere("dotenv")
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+const error = require("../middlewares/error");
+
+const secret = config.jwt.secret;
+
+function assignToken(data) {
+    return jwt.sign(data, secret);
+}
+
+function verifyToken(token) {
+    return jwt.verify(token, secret);
+}
+const checkToken = {
+    confirmToken: function (req, id, type) {
+        const decode = decodeHeader(req);
+        console.log(decode);
+        if (type === 0) {
+            if (decode.dataValues.id != id && decode.Roleid != '') {
+                throw new Error("You are not authorized to access this resource");
+            }
+        }
+    },
+};
+
+function getToken(authorization) {
+    if (!authorization) {
+        throw new Error("No authorization header provided");
+    }if(authorization.IndexOf('Bearer') === -1){
+        throw new Error("Invalid format");
+    }
+    let token = authorization.replace("Bearer ","");
+    return token;
+};
